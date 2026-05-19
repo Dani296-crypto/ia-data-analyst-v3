b0ced8df6eebce6f31f312e76e7cbb64ba34fb42
Dani296-crypto <abalod296@gmail.com>
2026-05-19 12:17:45 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,9 @@
 import streamlit as st
 
-st.set_page_config(page_title="Login IA", layout="centered")
+st.set_page_config(page_title="Login")
 
-# INIT STATE
 if "auth" not in st.session_state:
     st.session_state["auth"] = False
-
-# SI DEJA CONNECTE → aller dashboard
-if st.session_state["auth"]:
-    st.switch_page("ia")
-    st.stop()
 
 st.title("🔐 Login")
 
@@ -20,7 +14,7 @@ if st.button("Se connecter"):
     if password == st.secrets["APP_PASSWORD"]:
         st.session_state["auth"] = True
         st.success("Accès autorisé")
-        st.switch_page("ia")
+        st.rerun()
 
     else:
         st.error("Mot de passe incorrect")
